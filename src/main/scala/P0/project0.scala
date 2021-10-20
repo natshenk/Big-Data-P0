bc39c6a0fa721c86ee688c395e1af3982a28654b
natshenk <77948563+natshenk@users.noreply.github.com>
2021-10-20 01:59:45 -0400

    Added functionality for each command available in the user shell

--- src/main/scala/P0/project0.scala
+++ src/main/scala/P0/project0.scala
@@ -1,10 +1,12 @@
 package P0
 
 import org.mongodb.scala._
+import org.mongodb.scala.model.Filters._ // needed for equal()
 import scala.io.Source
 import net.liftweb.json._
 import java.io._
 import scala.io.StdIn._
+import scala.util.Random
 
 import P0.Helpers._
 
@@ -16,6 +18,8 @@ object Project0 {
    // Get a Collection.
    val collection: MongoCollection[Document] = database.getCollection("customerInfo")
 
+   var accountNum: Int = 164868000
+
    def main(args: Array[String]) {
       // Thread.sleep is used to allow the connection to MongoDB to occur before 
       // prompting the user (for console readability)
@@ -26,11 +30,12 @@ object Project0 {
       )
 
       importJSON()
+      println("\nNew data added successfully!")
 
       var exit = false
       while (!exit) {
          println(
-            "\nWelcome to the First National Bank of Mongo\n" +
+            "\nWelcome to the First National Bank of Mongo!\n" +
             "Please select an action from the list below by typing its number:\n" +
             "1 : Create an account\n" +
             "2 : Delete an account\n" +
@@ -42,21 +47,27 @@ object Project0 {
             "8 : Exit"
          )
 
-         val userSelection = readLine("Enter your selection #>")
+         val userSelection = readLine("Enter your selection # > ")
 
          userSelection match {
-            /*
             case "1" =>
+               addAccount()
             case "2" =>
+               deleteAccount()
             case "3" =>
+               updateAccount()
             case "4" =>
+               getSavings()
             case "5" =>
+               getChecking()
             case "6" =>
+               makeDeposit()
             case "7" =>
-            */
+               makeWithdrawal()
             case "8" => 
                println("See you next time!")
                exit = true
+               client.close()
             case _ => 
                println("Please enter a valid option 1-8")
          }
@@ -84,8 +95,8 @@ object Project0 {
          phone_num: String,
          state: String,
          cc_num: String,
-         savings: String,
-         checking: String
+         savings: Double,
+         checking: Double
       )
 
       var fileFound = false
@@ -96,7 +107,7 @@ object Project0 {
             // a FileNotFoundException occurs (in the catch block)
             fileFound = true
 
-            val dataPath = readLine("Please enter a filepath >")
+            val dataPath = readLine("Please enter a filepath > ")
 
             val json = parse(scala.io.Source.fromFile(dataPath).mkString)
 
@@ -127,5 +138,206 @@ object Project0 {
                fileFound = false
          } 
       }
+   }
+
+   /**
+     * Prompts the user for personal info, then assigns them an account and
+     * cc number as well as assigning their balance to be $0 before adding
+     * to the database
+     */
+   def addAccount() {
+      // Get personal info
+      val fname = readLine("First name > ")
+      val lname = readLine("Last name > ")
+      val email = readLine("Email > ")
+      val phone = readLine("Phone number > ")
+      val state = readLine("State > ")
+
+      // Increment the global account number tracker for a new, unique number
+      accountNum += 1
+
+      // Generate a random Mastercard card number
+      val rnd = new scala.util.Random
+      val cc: String = "5108750" + rnd.nextInt(999999999).toString()
+
+      // Turn the data into the proper Document format
+      val doc: Document = Document (
+         "account_num" -> accountNum.toString(),
+         "first_name" -> fname,
+         "last_name" -> lname,
+         "email" -> email,
+         "phone_num" -> phone,
+         "state" -> state,
+         "cc_num" -> cc,
+         "savings" -> 0.00,
+         "checking" -> 0.00
+      )
+
+      collection.insertOne(doc).results()
+      println(s"New account created! Your account number is $accountNum\n" +
+              s"Your new credit card with the number $cc is ready to be activated")
+
+      // Allow the user to see the success message before reprompting
+      Thread.sleep(5000)
+   }
+
+   def deleteAccount() {
+      val accountId = readLine("Account number to delete > ")
+      val confirm = readLine("Are you sure? (y/n) > ")
+      if (confirm == "y") {
+         collection.deleteOne(equal("account_num", accountId)).results()
+         println("Account deleted. We're sorry to see you go!")
+      } else {
+         println("Account deletion cancelled")
+      }
+      Thread.sleep(2000)
+   }
+
+   /**
+     * Prompts the user for their account number and what info they want to update
+     * before acquiring said new info and updating the matching document in Mongo
+     */
+   def updateAccount() {
+      import org.mongodb.scala.model.Updates._ // for set()
+
+      val accountId = readLine("Account number to update > ")
+
+      println(
+         "Select which info to change:\n" +
+         "1 : First name\n" +
+         "2 : Last name\n" +
+         "3 : Email\n" +
+         "4 : Phone number\n" +
+         "5 : State"
+      )
+      val selection = readLine("Enter your selection # > ")
+
+      // TODO: clean up redundant cases
+      selection match {
+         case "1" => 
+            val fname = readLine("Please enter new first name > ")
+            collection.updateOne(equal("account_num", accountId), set("first_name", fname)).results()
+            println("Account info updated")
+         case "2" => 
+            val lname = readLine("Please enter new last name > ")
+            collection.updateOne(equal("account_num", accountId), set("last_name", lname)).results()
+            println("Account info updated")
+         case "3" => 
+            val email = readLine("Please enter new email > ")
+            collection.updateOne(equal("account_num", accountId), set("email", email)).results()
+            println("Account info updated")
+         case "4" => 
+            val phone = readLine("Please enter new phone number > ")
+            collection.updateOne(equal("account_num", accountId), set("phone_num", phone)).results()
+            println("Account info updated")
+         case "5" => 
+            val state = readLine("Please enter new state > ")
+            collection.updateOne(equal("account_num", accountId), set("state", state)).results()
+            println("Account info updated")
+         case _ => 
+            println("Invalid option. Returning to main menu...")
+      }
+      // Allow the user to read the output
+      Thread.sleep(2000)
+   }
+
+   /**
+     * Gets the account ID from the user then returns just the savings balance
+     */
+   def getSavings() {
+      import org.mongodb.scala.model.Projections._
+
+      val accountId = readLine("Account number > ")
+
+      collection.find(equal("account_num", accountId))
+         .projection(fields(include("savings"), excludeId())).printHeadResult()
+      
+      Thread.sleep(3000)
+   }
+
+   /**
+     * Gets the account ID from the user then returns just the checking balance
+     */
+   def getChecking() {
+      import org.mongodb.scala.model.Projections._
+
+      val accountId = readLine("Account number > ")
+
+      collection.find(equal("account_num", accountId))
+         .projection(fields(include("checking"), excludeId())).printHeadResult()
+      
+      Thread.sleep(3000)
+   }
+
+   /**
+     * Gets the account ID to find the account then allows the user to
+     * choose their checking or savings account to deposit into along
+     * with how much before updating the document
+     */
+   def makeDeposit() {
+      import org.mongodb.scala.model.Updates._ // for set()
+
+      val accountId = readLine("Account number > ")
+
+      println(
+         "Select which account to deposit into:\n" +
+         "1 : Savings\n" +
+         "2 : Checking"
+      )
+      val selection = readLine("Enter your selection # > ")
+
+      // TODO: clean up redundant cases
+      selection match {
+         case "1" => 
+            print("How much to deposit? $")
+            val deposit = readDouble()
+            collection.updateOne(equal("account_num", accountId), inc("savings", deposit)).results()
+            println(s"$deposit added to savings account")
+         case "2" => 
+            print("How much to deposit? $")
+            val deposit = readDouble()
+            collection.updateOne(equal("account_num", accountId), inc("checking", deposit)).results()
+            println(s"$deposit added to checking account")
+         case _ => 
+            println("Invalid option. Returning to main menu...")
+      }
+      // Allow the user to read the output
+      Thread.sleep(2000)
+   }
+
+   /**
+     * Gets the account ID to find the account then allows the user to
+     * choose their checking or savings account to withdraw from along
+     * with how much before updating the document
+     */
+   def makeWithdrawal() {
+      import org.mongodb.scala.model.Updates._ // for set()
+
+      val accountId = readLine("Account number > ")
+
+      println(
+         "Select which account to withdraw from:\n" +
+         "1 : Savings\n" +
+         "2 : Checking"
+      )
+      val selection = readLine("Enter your selection # > ")
+
+      // TODO: clean up redundant cases
+      selection match {
+         case "1" => 
+            print("How much to withdraw? $")
+            val deposit = readDouble()
+            collection.updateOne(equal("account_num", accountId), inc("savings", -deposit)).results()
+            println(s"$deposit withdrawn from savings account")
+         case "2" => 
+            print("How much to withdraw? $")
+            val deposit = readDouble()
+            collection.updateOne(equal("account_num", accountId), inc("checking", -deposit)).results()
+            println(s"$deposit withdrawn from checking account")
+         case _ => 
+            println("Invalid option. Returning to main menu...")
+      }
+      // Allow the user to read the output
+      Thread.sleep(2000)
    }
 }
